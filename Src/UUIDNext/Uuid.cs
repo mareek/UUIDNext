103350ac7813faa912d8dd5bb575599952e90453
Matthieu Mourisson <mareek@gmail.com>
2021-08-16 19:14:49 +0200

    Created C# project and Solution

---
+++ Src/UUIDNext/Uuid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UUIDNext
+{
+    public static class Uuid
+    {
+        public static Guid NewV4() => throw new NotImplementedException();
+        public static Guid NewV6() => throw new NotImplementedException();
+        public static Guid NewV7() => throw new NotImplementedException();
+    }
+}
