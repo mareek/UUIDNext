9f2e8577a7335948e6520b8d2a85883926c1e64a
Matthieu Mourisson <mareek@gmail.com>
2023-01-22 10:30:08 +0100

    Added UuidV8 to the benchmark project

--- Src/UUIDNext.Benchmarks/UuidBench.cs
+++ Src/UUIDNext.Benchmarks/UuidBench.cs
@@ -2,10 +2,12 @@
 
 namespace UUIDNext.Benchmarks;
 
+[MemoryDiagnoser(false)]
 public class UuidBench
 {
     private static readonly Guid urlNamespaceId = Guid.Parse("6ba7b811-9dad-11d1-80b4-00c04fd430c8");
     private static readonly Generator.UuidV6Generator uuidV6Generator = new();
+    private static readonly Generator.UuidV8SqlServerGenerator uuidV8Generator = new();
 
     [Benchmark]
     public Guid NewGuid() => Guid.NewGuid();
@@ -21,4 +23,7 @@ public class UuidBench
 
     [Benchmark]
     public Guid NewUuidV7() => Uuid.NewSequential();
+
+    [Benchmark]
+    public Guid NewUuidV8() => uuidV8Generator.New();
 }
