cbda08a03a028c119dd99681878ab8e4cce906c0
Reza <reza.bakhshi@lifelenz.com>
2024-05-16 13:49:34 +0930

    reduce change footprint

--- Src/UUIDNext/Generator/UuidNameGeneratorBase.cs
+++ Src/UUIDNext/Generator/UuidNameGeneratorBase.cs
@@ -23,12 +23,13 @@ namespace UUIDNext.Generator
 
             //Compute the hash of the name space ID concatenated with the name.
             int bytesToHashCount = namespaceBytes.Length + utf8NameBytes.Length;
-
             byte[] bytesToHash = new byte[bytesToHashCount];
             namespaceBytes.CopyTo(bytesToHash);
             utf8NameBytes.CopyTo(bytesToHash, namespaceBytes.Length);
+
             HashAlgorithm hashAlgorithm = HashAlgorithm.Value;
             var hash = hashAlgorithm.ComputeHash(bytesToHash);
+
             return CreateGuidFromBigEndianBytes(hash.AsSpan(0 , 16));
 #else
             Span<byte> utf8NameBytes = (utf8NameByteCount > 256) ? new byte[utf8NameByteCount] : stackalloc byte[utf8NameByteCount];
@@ -40,13 +41,14 @@ namespace UUIDNext.Generator
 
             //Compute the hash of the name space ID concatenated with the name.
             int bytesToHashCount = namespaceBytes.Length + utf8NameBytes.Length;
-
             Span<byte> bytesToHash = (utf8NameByteCount > 256) ? new byte[bytesToHashCount] : stackalloc byte[bytesToHashCount];
             namespaceBytes.CopyTo(bytesToHash);
             utf8NameBytes.CopyTo(bytesToHash[namespaceBytes.Length..]);
+
             HashAlgorithm hashAlgorithm = HashAlgorithm.Value;
             Span<byte> hash = stackalloc byte[hashAlgorithm.HashSize / 8];
             hashAlgorithm.TryComputeHash(bytesToHash, hash, out var _);
+
             return CreateGuidFromBigEndianBytes(hash[0..16]);
 #endif
         }
