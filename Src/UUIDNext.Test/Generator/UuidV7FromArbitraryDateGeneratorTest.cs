09f70e5ad1cd7edc6df62fd184b7c9e2d5e5d87e
Matthieu Mourisson <mareek@gmail.com>
2024-09-09 22:34:03 +0200

    Created UuidV7FromArbitraryDateGenerator

---
+++ Src/UUIDNext.Test/Generator/UuidV7FromArbitraryDateGeneratorTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using NFluent;
+using UUIDNext.Generator;
+using UUIDNext.Tools;
+using Xunit;
+
+namespace UUIDNext.Test.Generator;
+
+public class UuidV7FromArbitraryDateGeneratorTest
+{
+    GuidComparer _comparer = new();
+
+    [Fact]
+    public void EnsureTimestampAndVersionAreAlwaysCorrect()
+    {
+        DateTimeOffset date = new(new(2020, 1, 1));
+        UuidV7FromArbitraryDateGenerator generator = new();
+
+        int overflowCount = 0;
+        Guid previousUuid = Uuid.Nil;
+
+        // we loop 10_000 times to make sure that the sequence of the UUID v7 will overflow at
+        // least twice since the sequence is stored on 12 bits and its maximum value is thus 4095
+        for (int i = 0; i < 10_000; i++)
+        {
+            var uuid = generator.New(date);
+            Check.That(UuidDecoder.GetVersion(uuid)).Is(7);
+            var (timestamp, sequence) = UuidDecoder.DecodeUuidV7(uuid);
+            Check.That(timestamp).Is(date.ToUnixTimeMilliseconds());
+
+            if (_comparer.Compare(previousUuid, uuid) > 0)
+                overflowCount++;
+
+            previousUuid = uuid;
+        }
+
+        Check.That(overflowCount).IsGreaterOrEqualThan(2).And.IsLessOrEqualThan(5);
+    }
+
+    [Fact]
+    public void EnsureThatUuidsFromTheSameTimestampAreIncreasing()
+    {
+        DateTimeOffset date = new(new(2020, 1, 1));
+        UuidV7FromArbitraryDateGenerator generator = new();
+        Guid previousUuid = Uuid.Nil;
+        for (int i = 0; i < 100; i++)
+        {
+            var uuid = generator.New(date);
+            Check.That(_comparer.Compare(previousUuid, uuid)).IsStrictlyLessThan(0);
+            previousUuid = uuid;
+        }
+    }
+
+    [Fact]
+    public void EnsureThatUuidsFromMultipleTimestampAreIncreasing()
+    {
+        DateTimeOffset dateT = new(new(2008, 10, 12));
+        DateTimeOffset dateA = new(new(2010, 5, 1));
+        DateTimeOffset dateS = new(new(2017, 11, 24));
+
+        UuidV7FromArbitraryDateGenerator generator = new();
+
+        Guid previousUuidT = Uuid.Nil;
+        Guid previousUuidA = Uuid.Nil;
+        Guid previousUuidS = Uuid.Nil;
+
+        for (int i = 0; i < 100; i++)
+        {
+            var uuidT = generator.New(dateT);
+            var uuidA = generator.New(dateA);
+            var uuidS = generator.New(dateS);
+
+            Check.That(_comparer.Compare(previousUuidT, uuidT)).IsStrictlyLessThan(0);
+            Check.That(_comparer.Compare(previousUuidA, uuidA)).IsStrictlyLessThan(0);
+            Check.That(_comparer.Compare(previousUuidS, uuidS)).IsStrictlyLessThan(0);
+
+            previousUuidT = uuidT;
+            previousUuidA = uuidA;
+            previousUuidS = uuidS;
+        }
+    }
+}
--- Src/UUIDNext/Generator/UuidV7FromArbitraryDateGenerator.cs
+++ Src/UUIDNext/Generator/UuidV7FromArbitraryDateGenerator.cs
@@ -7,21 +7,30 @@ namespace UUIDNext.Generator;
 /// Generate a UUID version 7 given an arbitrary date
 /// </summary>
 /// <remarks>
-/// To give the best possible UUID given an arbitrary date we can't rely on the same mechanism as in UuidV7Generator
+/// To give the best possible UUID given an arbitrary date we can't rely on UuidV7Generator because it has some 
+/// mechanism to ensure that every UUID generated is greater then the previous one.
 /// </remarks>
 internal class UuidV7FromArbitraryDateGenerator
 {
     private QDCache<long, MonotonicityHandler> _monotonicityHandlerByTimestamp = new(100);
 
-    private Guid New(DateTimeOffset date)
+    /// <summary>
+    /// Create a UUID version 7 where the timestamp part represent the given date
+    /// </summary>
+    /// <param name="date">The date that will provide the timestamp par of the UUID</param>
+    /// <returns>A UUID version 7</returns>
+    public Guid New(DateTimeOffset date)
     {
         var dateTimeStamp = date.ToUnixTimeMilliseconds();
         var monotonicityHandler = _monotonicityHandlerByTimestamp.GetOrAdd(dateTimeStamp, CreateV7Handler);
         var (computedTimestamp, sequence) = monotonicityHandler.GetTimestampAndSequence(date);
 
+        // if the timestamp given by the monotonicityHandler is greater than the the date parameter that means that 
+        // the sequence have overflowed so we reset the sequence by creating a new monotonicityHandler
         if (computedTimestamp > dateTimeStamp)
         {
-            _monotonicityHandlerByTimestamp.Set(dateTimeStamp, CreateV7Handler(default));
+            monotonicityHandler = CreateV7Handler(default);
+            _monotonicityHandlerByTimestamp.Set(dateTimeStamp, monotonicityHandler);
             (computedTimestamp, sequence) = monotonicityHandler.GetTimestampAndSequence(date);
         }
 
