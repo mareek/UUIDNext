ec6eb8f2ddc2b8eaffcd54f0cb065fcc86fb2ee3
Matthieu Mourisson <mareek@gmail.com>
2024-09-29 22:01:45 +0200

    Removed now useless set method from QDCache

--- Src/UUIDNext.Test/Tools/QDCacheTest.cs
+++ Src/UUIDNext.Test/Tools/QDCacheTest.cs
@@ -39,39 +39,6 @@ public class QDCacheTest
     }
 
     [Fact]
-    public void EnsureSetMethodWorks()
-    {
-        int factoryCount = 0;
-        QDCache<string, int> cache = new(5);
-
-        cache.Set("first", -1);
-        var first = cache.GetOrAdd("first", _ => ++factoryCount);
-        Check.That(first).Is(-1);
-
-        cache.Set("second", -2);
-        var second = cache.GetOrAdd("second", _ => ++factoryCount);
-        Check.That(second).Is(-2);
-
-
-        for (int i = 0; i < 10; i++)
-        {
-            var other = cache.GetOrAdd($"{i}", _ => ++factoryCount);
-            Check.That(other).Is(factoryCount);
-            cache.Set("first", 0); // this line keeps "first" in the cache
-        }
-
-        // "first" is still in the cache
-        first = cache.GetOrAdd("first", _ => ++factoryCount);
-        Check.That(first).Is(0);
-
-        // "second" has been driven out of the cache by the for loop
-        second = cache.GetOrAdd("second", _ => ++factoryCount);
-        Check.That(second).Is(factoryCount);
-
-        Check.That(factoryCount).Is(11);
-    }
-
-    [Fact]
     public void EnsureAddOrUpdateMethodWorks()
     {
         QDCache<string, int> cache = new(5);
