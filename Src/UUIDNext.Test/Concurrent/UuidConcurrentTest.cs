75e9427a2a9bf3172177d2c11cf7990763be3646
Matthieu Mourisson <mareek@gmail.com>
2026-03-30 00:20:55 +0200

    Create test to reproduce #40

---
+++ Src/UUIDNext.Test/Concurrent/UuidConcurrentTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using NFluent;
+using UUIDNext.Tools;
+using Xunit;
+
+namespace UUIDNext.Test.Concurrent;
+
+public class UuidConcurrentTest
+{
+    [Theory]
+    [InlineData(8, 16_384)]
+    public void SpecificDateStressTest(int threadCount, int dateCount)
+    {
+        int exceptionCount = 0;
+        DateTimeOffset baseDate = new(new(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+        var threads = new Thread[threadCount];
+        for (int i = 0; i < threadCount; i++)
+            threads[i] = new(Action);
+
+        for (int i = 0; i < threadCount; i++)
+            threads[i].Start();
+
+        for (int i = 0; i < threadCount; i++)
+            threads[i].Join();
+
+        Check.That(exceptionCount).IsZero();
+
+        void Action()
+        {
+            for (int dateOffset = 0; dateOffset < dateCount; dateOffset++)
+                try
+                {
+                    var date = baseDate.AddMinutes(dateOffset);
+                    var uuid = UuidToolkit.CreateUuidV7FromSpecificDate(date);
+                }
+                catch (Exception)
+                {
+                    Interlocked.Increment(ref exceptionCount);
+                    return;
+                }
+        }
+    }
+}
